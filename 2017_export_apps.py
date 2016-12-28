cceb5dbb8933f22daf0f7df58fd3a82cb15c0eff
Schmill731 <williamschmitt@college.harvard.edu>
2016-12-28 14:53:08 -0500

    added readQualtricsCSV function

--- 2017_export_apps.py
+++ 2017_export_apps.py
@@ -20,11 +20,34 @@
 # Import necessary libraries
 import csv
 
-#global variables
-apps = None
-junkHeaders = ["ResponseSet", "BlankName", "ExternalDataReference", "BlankEmail",
-    "IPAddress", "Status", "StartDate", "EndDate", "Finished", "ResponseID",
-    "Intro", "Ones", "RecInfo", "Lat", "Long", "LocAcc", "Blank"]
+# Functions
+def readQualtricsCSV(filePath, header, uniqueJunk=[]):
+    """Reads in Exported Qualtrics CSV files located at filePath, uses header to
+    build a list of dictionaries where the keys are header, and then strips away
+    the data located at uniqueJunk plus the common headers across all Qualtrics
+    CSV exports."""
+
+    #Read in file
+    with open(filePath, newline='') as qualcsv:
+        rawData = csv.DictReader(qualcsv, fieldnames=header)
+
+        #Remove Qualtrics headers
+        responses = list(rawData)[2:]
+
+        #Common junk to all Qualtrics CSV exports
+        commonJunk = ["ResponseSet", "BlankName", "ExternalDataReference", "BlankEmail",
+            "IPAddress", "Status", "StartDate", "EndDate", "Finished",
+            "Lat", "Long", "LocAcc", "Blank"]
+
+        #Combine with commonJunk with unique junk
+        junkHeaders = commonJunk + uniqueJunk
+
+        #Remove junk
+        for response in responses:
+            for junk in junkHeaders:
+                response.pop(junk)
+
+        return responses
 
 # Create headers for registration data
 regHeader = ["RegID", "ResponseSet", "BlankName", "ExternalDataReference", 
@@ -33,22 +56,24 @@ regHeader = ["RegID", "ResponseSet", "BlankName", "ExternalDataReference",
     "Rec1First", "Rec1Last", "Rec1Email", "Rec2First", "Rec2Last", "Rec2Email",
     "Rec3First", "Rec3Last", "Rec3Email", "Rec4First", "Rec4Last", "Rec4Email",
     "Lat", "Long", "LocAcc", "Blank"]
-
-
+regJunk = ["ResponseID", "Intro", "Ones", "RecInfo"]
 
 # Read in Registration Data
-with open('../Summer_Course_2017_Registration.csv', newline='') as regFile:
-    registrations = csv.DictReader(regFile, fieldnames=regHeader)
-    
-    # Remove the Qualtrics headers
-    apps = list(registrations)[2:]
+regs = readQualtricsCSV("../Summer_Course_2017_Registration.csv", regHeader, regJunk)
 
-    # Remove the unncessary elements
-    for app in apps:
-        for junk in junkHeaders:
-            app.pop(junk)
+#Create headers for application data
+appHeader = ["AppID", "ResponseSet", "BlankName", "ExternalDataReference",
+    "BlankEmail", "IPAddress", "Status", "StartDate", "EndDate", "Finished",
+    "FirstName", "LastName", "Email", "SOI", "TranscriptURL", "cvURL", "Lat",
+    "Long", "LocAcc", "Blank"]
 
-print("DEBUG:")
+# Read in Application Data
+apps = readQualtricsCSV("../Summer_Course_2017_Application.csv", appHeader)
+
+print("DEBUG: regs")
+print(regs)
+print("DEBUG: apps")
 print(apps)
+
 
 
